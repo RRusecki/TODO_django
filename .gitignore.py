729a08bf376b389a80c2945dfcbc5804c38334c4
RRusecki <robertr19941@gmail.com>
2025-07-07 13:51:55 +0300

    Pradinis TODO

---
+++ .gitignore.py
@@ -0,0 +1,23 @@
+# Python
+__pycache__/
+*.py[cod]
+*.sqlite3
+
+# Django
+*.log
+*.pot
+*.pyc
+db.sqlite3
+
+# Env
+.env
+.venv/
+venv/
+
+# VSCode / PyCharm
+.idea/
+*.iml
+
+# Mac/Linux/Windows sistemos failai
+.DS_Store
+Thumbs.db
