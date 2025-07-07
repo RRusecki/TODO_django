95e44df70882a1dc80b1c9e06fa857727c8f61ce
RRusecki <robertr19941@gmail.com>
2025-07-07 20:02:43 +0300

    veikia logout

--- main/urls.py
+++ main/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("todo.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
