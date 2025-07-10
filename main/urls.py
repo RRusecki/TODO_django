cbb3d8eb946325b5dcb8497b75ee0bcaf4e55226
RRusecki <robertr19941@gmail.com>
2025-07-10 08:02:55 +0300

    sd

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -24,4 +25,4 @@ urlpatterns = [
     path("", include("todo.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
